677e9aad815650fb0bf81e8158a3fc0d1c4f45ba
Surjeet Mohanty <surjeetmohanty2019@gmail.com>
2020-05-14 01:00:31 +0530

    changes

--- src/main/java/com/c2p/test/TestApplication.java
+++ src/main/java/com/c2p/test/TestApplication.java
@@ -1,30 +1,23 @@
 package com.c2p.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-//@SpringBootApplication
-//@ComponentScan(basePackages = {"com.c2p.test","com.c2p.config"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.c2p.test","com.c2p.config"})
 public class TestApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(TestApplication.class, args);
-		List<JSONObject> list = new ArrayList<JSONObject>();
-		String string="[{\"from\":\"2021-01-30\",\"to\":\"2021-05-30\"}]";
-		try {
-		    int i;
-		    JSONArray array = new JSONArray(string);
-		    for (i = 0; i < array.length(); i++) {
-		        list.add(array.getJSONObject(i));
-		    }
-		    System.out.println(list);
-		} catch (JSONException e) {
-		    System.out.println(e.getMessage());
-		}
+		SpringApplication.run(TestApplication.class, args);
+		
+		/*
+		 * List<JSONObject> list = new ArrayList<JSONObject>(); String
+		 * string="[{\"from\":\"2021-01-30\",\"to\":\"2021-05-30\"}]"; try { int i;
+		 * JSONArray array = new JSONArray(string); for (i = 0; i < array.length(); i++)
+		 * { list.add(array.getJSONObject(i)); } System.out.println(list); } catch
+		 * (JSONException e) { System.out.println(e.getMessage()); }
+		 */
 	}
 
 }
