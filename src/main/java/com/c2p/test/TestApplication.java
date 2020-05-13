6f9ae4cb5ac0b88f4aa237f383faaa884cd79554
Surjeet Mohanty <surjeetmohanty2019@gmail.com>
2020-05-14 00:57:37 +0530

    add

--- src/main/java/com/c2p/test/TestApplication.java
+++ src/main/java/com/c2p/test/TestApplication.java
@@ -1,15 +1,30 @@
 package com.c2p.test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.c2p.test","com.c2p.config"})
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//@SpringBootApplication
+//@ComponentScan(basePackages = {"com.c2p.test","com.c2p.config"})
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		//SpringApplication.run(TestApplication.class, args);
+		List<JSONObject> list = new ArrayList<JSONObject>();
+		String string="[{\"from\":\"2021-01-30\",\"to\":\"2021-05-30\"}]";
+		try {
+		    int i;
+		    JSONArray array = new JSONArray(string);
+		    for (i = 0; i < array.length(); i++) {
+		        list.add(array.getJSONObject(i));
+		    }
+		    System.out.println(list);
+		} catch (JSONException e) {
+		    System.out.println(e.getMessage());
+		}
 	}
 
 }
